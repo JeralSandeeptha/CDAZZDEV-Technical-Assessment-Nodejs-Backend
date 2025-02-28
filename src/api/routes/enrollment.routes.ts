193a6312381c61b65005a1e88ee9c20014383744
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-03-01 02:23:42 +0530

    added get enrollments by student id

--- src/api/routes/enrollment.routes.ts
+++ src/api/routes/enrollment.routes.ts
@@ -1,11 +1,12 @@
 import { verifyAdmin, verifyToken } from '../middlewares/userAuthenticator';
-import { createEnrollment, deleteEnrollment, getEnrollment, getEnrollments, updateEnrollment } from '../controllers/enrollment.controller';
+import { createEnrollment, deleteEnrollment, getEnrollment, getEnrollments, getEnrollmentsBtStudentId, updateEnrollment } from '../controllers/enrollment.controller';
 import express from 'express';
 
 const router = express.Router();
 
 router.post('/', verifyToken, createEnrollment);
 router.get('/', verifyToken, getEnrollments);
+router.get('/get-enrollments-by-studentId/:studentId', verifyToken, getEnrollmentsBtStudentId);
 router.get('/:enrollmentId/:courseId/:studentId', verifyToken, getEnrollment);
 router.delete('/:enrollmentId', verifyAdmin, deleteEnrollment);
 router.put('/:enrollmentId', verifyAdmin, updateEnrollment);
